f1e6d8b80312ba3714ed0874386507e35e8422fc
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 21:41:48 +0100

    Fixed rst again

--- build.bat
+++ build.bat
@@ -2,8 +2,8 @@
 
 python setup.py bdist_wheel sdist
 sphinx-apidoc -F -M -o docs pyunity
+sphinx-build -b html docs docs/_html
 git add .
 git commit -m %1
 git push
-"docs/make" html
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
