62d5f9b21b9daca20f5c41948c58a9fd94ac4b08
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 10:14:36 +0100

    Finished setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import setup
+import pyunity
 
 setup(
     name = "pyunity",
-    
+    version = pyunity.__version__,
+    description = "",
+    url = "",
+    install_requires = [
+        "glfw",
+        "pygame",
+        "pyopengl",
+    ],
 )
