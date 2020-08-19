65d71b38ee37571c2be5e4f1370b47581aabbb2d
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 20:37:33 +0100

    Added docstrings for gameobject

---
+++ build/lib/pyunity/errors.py
@@ -0,0 +1,8 @@
+class PyUnityException(Exception):
+    pass
+
+class ComponentException(PyUnityException):
+    pass
+
+class GameObjectException(PyUnityException):
+    pass
---
+++ build/lib/pyunity/window/__init__.py
@@ -0,0 +1,3 @@
+from .glfwWindow import Window as glfwWindow
+from .glutWindow import Window as glutWindow
+from .pygameWindow import Window as pygameWindow
