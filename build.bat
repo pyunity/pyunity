a8d2b470f1cc5261df87cdf21f0eb7e1c2efbe3d
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 21:18:11 +0100

    Updated build scripts

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 
 python setup.py bdist_wheel sdist
-pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
 git add .
 git commit -m %1
 git push
+pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
