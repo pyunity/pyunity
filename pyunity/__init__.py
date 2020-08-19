7ec1b06fed843dc6e703b1b9cf4fde58b7b2b8bf
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 19:37:41 +0100

    Fixed pygame welcome message

--- pyunity/__init__.py
+++ pyunity/__init__.py
@@ -1,8 +1,8 @@
+from . import config
 from .core import *
 from .vector3 import Vector3
 from .scene import SceneManager
 from . import meshes
-from . import config
 from . import errors
 
 __version__ = "0.0.1"
@@ -11,22 +11,26 @@ try:
     from OpenGL.GLUT import *
     glutInit()
     config.windowProvider = "glut"
+    w = "FreeGLUT"
 except Exception as e:
     print("GLUT doesn't work, using GLFW")
-    import glfw
     try:
+        import glfw
         if not glfw.init():
             raise Exception
         glfw.create_window(50, 50, "Test", None, None)
         glfw.terminate()
         config.windowProvider = "glfw"
+        w = "GLFW"
     except Exception as e:
         print("GLFW doesn't work, using Pygame")
         import pygame
-        if not pygame.init():
+        if pygame.init()[0] == 0:
             raise PyUnityException("No window provider found")
         config.windowProvider = "pygame"
+        w = "Pygame"
 
 SceneManager = SceneManager()
 
 print(f"Loaded PyUnity version {__version__}")
+print(f"Using window provider {w}")
