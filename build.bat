41cd4ec5bd2e4fb2ef674e3c617dba4de406e1d3
Ray Chen <tankimarshal2@gmail.com>
2020-08-19 21:35:38 +0100

    Fixed rtd

--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 @ECHO OFF
 
 python setup.py bdist_wheel sdist
+sphinx-apidoc -F -M -o docs pyunity
 git add .
 git commit -m %1
 git push
+"docs/make" html
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
