eb8fe9a036f9d03f19e637729160af0729ea1799
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 10:29:27 +0100

    Fixing rtd so much...

--- build.bat
+++ build.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 
 python setup.py bdist_wheel sdist
+pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
 sphinx-apidoc -F -M -o docs pyunity
 sphinx-build -b html docs docs/_build/_html
-if %1 == "-c" (
 git add .
-git commit -m %2
+git commit -m %1
 git push
-)
-pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
