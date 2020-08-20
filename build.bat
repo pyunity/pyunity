9535c7436567af7de79299b11dab97d1e07523aa
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 10:47:58 +0100

    Excluding config.py

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 python setup.py bdist_wheel sdist
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
-sphinx-apidoc -e -F -M -o docs pyunity config.py
+sphinx-apidoc -E -F -M -o docs pyunity config.py
 sphinx-build -b html docs docs/_build/_html
 git add .
 git commit -m %1
