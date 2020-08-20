9eddea5195651c4f86719106bb15c4683d41a6d1
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 15:56:17 +0100

    Removed workflow

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-import pyunity
 sys.path.insert(0, os.path.abspath('../..'))
+import pyunity
 
 
 # -- Project information -----------------------------------------------------
---
+++ release.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+
+twine upload --repository testpypi dist/*
