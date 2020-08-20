83da87e88a1fcb39cec827ac29def14efecf4022
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 11:28:20 +0100

    Used sphinx-quickstart

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@
 python setup.py bdist_wheel sdist
 RMDIR /S /Q build /S /Q pyunity.egg-info
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
-sphinx-apidoc -F -M -o docs pyunity config.py
+sphinx-apidoc -F -M -o docs/source pyunity
 sphinx-build -b html docs docs/_build/_html
 git add .
 git commit -m %1
