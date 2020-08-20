9fd9fffbe837010602c914f6fde64cbbce7f763f
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 16:26:29 +0100

    Fixed No module named 'glfw' error when building docs

--- pyunity/window/__init__.py
+++ pyunity/window/__init__.py
@@ -1,7 +1,7 @@
 import os
-from .glfwWindow import Window as glfwWindow
-from .glutWindow import Window as glutWindow
 from .pygameWindow import Window as pygameWindow
+from .glutWindow import Window as glutWindow
+from .glfwWindow import Window as glfwWindow
 
 if os.environ["PYUNITY_DEBUG_MODE"] == "1":
     print("Loaded window providers")
