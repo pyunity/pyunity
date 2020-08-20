80222451c93a3d3876773f34ee9c72f94b270623
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 10:21:23 +0100

    Fixed rtd and added NumPy docstrings

--- build.bat
+++ build.bat
@@ -3,7 +3,9 @@
 python setup.py bdist_wheel sdist
 sphinx-apidoc -F -M -o docs pyunity
 sphinx-build -b html docs docs/_build/_html
+if %1 == "-c" (
 git add .
-git commit -m %1
+git commit -m %2
 git push
+)
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath("../pyunity"))
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../pyunity"))
 
 
 # -- Project information -----------------------------------------------------
@@ -33,6 +33,7 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.viewcode",
     "sphinx.ext.todo",
+    "sphinx.ext.napoleon",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
