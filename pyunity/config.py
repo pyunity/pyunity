cf5a224d85a7083450f630ed33b2708df4674336
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 15:42:31 +0100

    Version 0.0.2

--- pyunity/config.py
+++ pyunity/config.py
@@ -1,5 +1,7 @@
 import os
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+if "PYUNITY_DEBUG_MODE" not in os.environ: os.environ["PYUNITY_DEBUG_MODE"] = "1"
+
 from .window import *
 
 windowProvider = None
@@ -8,3 +10,6 @@ fps = 60
 faceCulling = True
 
 windowProviders = {"glut": glutWindow, "glfw": glfwWindow, "pygame": pygameWindow}
+
+if os.environ["PYUNITY_DEBUG_MODE"] == "1":
+    print("Loaded config")
