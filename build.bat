67abb852d13354df1ef28755dac09216c57d9be6
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 11:13:46 +0100

    Fix docs

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 python setup.py bdist_wheel sdist
+RMDIR /S /Q build /S /Q pyunity.egg-info
 pip install --upgrade dist/pyunity-0.0.1-py3-none-any.whl
 sphinx-apidoc -F -M -o docs pyunity config.py
 sphinx-build -b html docs docs/_build/_html
