a547812e3f2f7f8b209733a752d5baf44240c8de
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 13:55:18 +0100

    Fixed test

--- pyunity/tests/test1.py
+++ pyunity/tests/test1.py
@@ -9,7 +9,7 @@ def main():
 
     cube = GameObject("Cube")
     renderer = cube.AddComponent(MeshRenderer)
-    renderer.mesh = Mesh.cube
+    renderer.mesh = Mesh.cube()
     renderer.mat = Material((255, 0, 0))
     cube.AddComponent(Rotator)
 
