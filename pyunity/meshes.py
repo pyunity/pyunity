cd695965d898197a6aed4c4f4889e1109d5e3917
Ray Chen <tankimarshal2@gmail.com>
2020-08-20 12:11:32 +0100

    Created static methods and docstrings for the meshes

--- pyunity/meshes.py
+++ pyunity/meshes.py
@@ -1,58 +1,82 @@
 from .vector3 import Vector3
 
 class Mesh:
+    """
+    Class to create a mesh for rendering with a MeshRenderer
+
+    Parameters
+    ----------
+    verts : list
+        List of Vector3's containing each vertex
+    triangles : list
+        List of ints containing triangles joining up the vertexes.
+        Each int is the index of a vertex above.
+    normals : list
+        List of Vector3's containing the normal of each triangle.
+        Unlike Unity, PyUnity uses normals per triangle.
+
+    Attributes
+    ----------
+    Same as Parameters
+    
+    """
+
     def __init__(self, verts, triangles, normals):
         self.verts = verts
         self.triangles = triangles
         self.normals = normals
+    
+    @staticmethod
+    def quad(self, size):
+        """
+        Creates a quadrilateral mesh.
 
-Mesh.quad = Mesh(
-    [
-        Vector3(1, 1, 0),
-        Vector3(-1, 1, 0),
-        Vector3(-1, -1, 0),
-        Vector3(1, -1, 0),
-    ],
-    [
-        [0, 1, 2],
-        [0, 2, 3],
-    ],
-    [
-        Vector3(0, 0, -1),
-        Vector3(0, 0, -1)
-    ]
-)
+        Parameters
+        ----------
+        size : float
+            Side length of quad
 
-Mesh.cube = Mesh(
-    [
-        Vector3(x, y, z) for x in [-1, 1] for y in [-1, 1] for z in [-1, 1]
-    ],
-    [
-        [0, 2, 1],
-        [1, 2, 3],
-        [4, 5, 6],
-        [5, 7, 6],
-        [0, 4, 2],
-        [2, 4, 6],
-        [1, 3, 5],
-        [3, 7, 5],
-        [2, 6, 3],
-        [3, 6, 7],
-        [0, 1, 4],
-        [4, 1, 5],
-    ],
-    [
-        Vector3(-1, 0, 0),
-        Vector3(-1, 0, 0),
-        Vector3(1, 0, 0),
-        Vector3(1, 0, 0),
-        Vector3(0, 0, 1),
-        Vector3(0, 0, 1),
-        Vector3(0, 0, -1),
-        Vector3(0, 0, -1),
-        Vector3(0, 1, 0),
-        Vector3(0, 1, 0),
-        Vector3(0, -1, 0),
-        Vector3(0, -1, 0),
-    ]
-)
+        Returns
+        -------
+        Mesh
+            A quad centered at Vector3(0, 0) with side length of `size` 
+            facing in the direction of the negative z axis.
+        
+        """
+        return Mesh(
+            [
+                Vector3(size / 2, size / 2, 0), Vector3(-size / 2, size / 2, 0),
+                Vector3(-size / 2, -size / 2, 0), Vector3(size / 2, -size / 2, 0)
+            ],
+            [[0, 1, 2], [0, 2, 3]],
+            [Vector3(0, 0, -1), Vector3(0, 0, -1)]
+        )
+    
+    @staticmethod
+    def cube(self, size):
+        """
+        Creates a cube mesh.
+
+        Parameters
+        ----------
+        size : float
+            Side length of cube
+
+        Returns
+        -------
+        Mesh
+            A cube centered at Vector3(0, 0, 0) that has a side length of `size`
+        
+        """
+        return Mesh(
+            [Vector3(x, y, z) for x in [-size / 2, size / 2] for y in [-size / 2, size / 2] for z in [-size / 2, size / 2]],
+            [
+                [0, 2, 1], [1, 2, 3], [4, 5, 6], [5, 7, 6], [0, 4, 2], [2, 4, 6],
+                [1, 3, 5], [3, 7, 5], [2, 6, 3], [3, 6, 7], [0, 1, 4], [4, 1, 5],
+            ],
+            [
+                Vector3(-1, 0, 0), Vector3(-1, 0, 0), Vector3(1, 0, 0), Vector3(1, 0, 0),
+                Vector3(0, 0, 1), Vector3(0, 0, 1), Vector3(0, 0, -1), Vector3(0, 0, -1),
+                Vector3(0, 1, 0), Vector3(0, 1, 0), Vector3(0, -1, 0), Vector3(0, -1, 0),
+            ]
+        )
