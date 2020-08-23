129a8af4e2b156425e2f13e6eb46fe1b28897d93
Ray Chen <tankimarshal2@gmail.com>
2020-08-23 17:59:43 +0100

    Remove no interactive

--- pyunity/config.py
+++ pyunity/config.py
@@ -1,6 +1,7 @@
 import os
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
 if "PYUNITY_DEBUG_MODE" not in os.environ: os.environ["PYUNITY_DEBUG_MODE"] = "1"
+if "PYUNITY_NO_INTERACTIVE" not in os.environ: os.environ["PYUNITY_NO_INTERACTIVE"] = "0"
 
 size = (800, 500)
 fps = 60
@@ -9,43 +10,48 @@ faceCulling = True
 if os.environ["PYUNITY_DEBUG_MODE"] == "1":
     print("Loaded config")
 
-from . import window
+if os.environ["PYUNITY_NO_INTERACTIVE"] == "0":
 
-try:
-    if os.environ["PYUNITY_DEBUG_MODE"] == "1":
-        print("Trying FreeGLUT as a window provider")
-    import OpenGL.GLUT
-    OpenGL.GLUT.glutInit()
-    del OpenGL
-    windowProvider = "glut"
-    w = "FreeGLUT"
-except Exception as e:
-    if os.environ["PYUNITY_DEBUG_MODE"] == "1":
-        print("FreeGLUT doesn't work, using GLFW")
+    from . import window
+
     try:
-        import glfw
-        if not glfw.init():
-            raise Exception
-        glfw.create_window(50, 50, "Test", None, None)
-        glfw.terminate()
-        del glfw
-        windowProvider = "glfw"
-        w = "GLFW"
+        if os.environ["PYUNITY_DEBUG_MODE"] == "1":
+            print("Trying FreeGLUT as a window provider")
+        import OpenGL.GLUT
+        OpenGL.GLUT.glutInit()
+        del OpenGL
+        windowProvider = "glut"
+        w = "FreeGLUT"
     except Exception as e:
         if os.environ["PYUNITY_DEBUG_MODE"] == "1":
-            print("GLFW doesn't work, using Pygame")
-        import pygame
-        if pygame.init()[0] == 0:
-            raise PyUnityException("No window provider found")
-        del pygame
-        windowProvider = "pygame"
-        w = "Pygame"
+            print("FreeGLUT doesn't work, using GLFW")
+        try:
+            import glfw
+            if not glfw.init():
+                raise Exception
+            glfw.create_window(50, 50, "Test", None, None)
+            glfw.terminate()
+            del glfw
+            windowProvider = "glfw"
+            w = "GLFW"
+        except Exception as e:
+            if os.environ["PYUNITY_DEBUG_MODE"] == "1":
+                print("GLFW doesn't work, using Pygame")
+            import pygame
+            if pygame.init()[0] == 0:
+                raise PyUnityException("No window provider found")
+            del pygame
+            windowProvider = "pygame"
+            w = "Pygame"
 
-windowProvider = window.LoadWindowProvider(windowProvider)
+    windowProvider = window.LoadWindowProvider(windowProvider)
 
-del window
+    del window
 
-if os.environ["PYUNITY_DEBUG_MODE"] == "1":
-    print(f"Using window provider {w}")
+    if os.environ["PYUNITY_DEBUG_MODE"] == "1":
+        print(f"Using window provider {w}")
+
+else:
+    print("PYUNITY_NO_INTERACTIVE is set to 1, no window provider loaded")
 
 del os
