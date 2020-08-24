420c8670d8a2b4d3d18dbbe39797bec5506e8521
Ray Chen <tankimarshal2@gmail.com>
2020-08-24 17:36:03 +0100

    Start of version 0.0.4

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-twine upload --repository testpypi dist/pyunity-0.0.3*
-twine upload dist/pyunity-0.0.3*
+twine upload --repository testpypi dist/pyunity-0.0.4*
+twine upload dist/pyunity-0.0.4*
