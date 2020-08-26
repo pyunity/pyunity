d0dc2a8f5db3b20ed234677f6f8b64b5c7a420a3
Ray Chen <tankimarshal2@gmail.com>
2020-08-26 21:52:49 +0100

    Never mind installing

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-python setup.py install bdist_wheel sdist
+python setup.py bdist_wheel sdist
 RMDIR /S /Q "build/" /S /Q "pyunity.egg-info/" /S /Q "docs/build/html/"
 sphinx-apidoc -e -F -M -o docs/source pyunity pyunity/config.py pyunity/examples/*
 sphinx-build -T -E -b html docs/source docs/build/html
