d7d215b3a356048d9d8cdabcce993cb7699e7624
Ray Chen <tankimarshal2@gmail.com>
2020-09-10 20:54:20 +0100

    Edited build.bat

--- build.bat
+++ build.bat
@@ -4,6 +4,8 @@ python setup.py bdist_wheel sdist
 RMDIR /S /Q "build/" /S /Q "pyunity.egg-info/" /S /Q "docs/build/html/"
 sphinx-apidoc -e -F -M -o docs/source pyunity pyunity/physics/config.py pyunity/config.py pyunity/examples/*
 sphinx-build -T -E -b html docs/source docs/build/html
+IF NOT [%1] == [] (
 git add .
 git commit -m %1
 git push
+)
