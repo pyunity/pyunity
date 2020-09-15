bdfd1bd5836c4cb87b52608d97eba3a2ed7174e9
Ray Chen <tankimarshal2@gmail.com>
2020-09-15 21:08:33 +0100

    Added cython help for mingw

--- build.bat
+++ build.bat
@@ -1,18 +1,10 @@
 @ECHO OFF
 
-IF NOT [%1] == [-x] (
-SET CYTHON=1
-DEL /s *.pyd
-DEL /s *.so
-SET MSG=%2
-) ELSE SET MSG=%1
-python setup.py bdist_wheel sdist
-IF [%1] == [-x] SET CYTHON=
-RMDIR /S /Q "build/" /S /Q "pyunity.egg-info/" /S /Q "docs/build/html/"
+python setup.py build -c mingw32 bdist_wheel -b "dist/0.0.6/" sdist
+RMDIR /S /Q "build/" "pyunity.egg-info/" "docs/build/html/"
 sphinx-build -T -E -b html docs/source docs/build/html
-IF NOT [%MSG%] == [] (
+IF NOT [%1] == [] (
 git add .
-git commit -m %MSG%
+git commit -m %1
 git push
 )
-set MSG=
