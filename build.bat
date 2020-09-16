587a9a3a6f32e06e8ec5701907bd50116adf1c41
Ray Chen <tankimarshal2@gmail.com>
2020-09-16 16:05:21 +0100

    Edited build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-python setup.py build -c mingw32 bdist_wheel -b "dist/0.0.6/" sdist
+python setup.py build -c mingw32 bdist_wheel -d "dist/0.0.6/" sdist -d "dist/0.0.6/"
 RMDIR /S /Q "build/" "pyunity.egg-info/" "docs/build/html/"
 sphinx-build -T -E -b html docs/source docs/build/html
 IF NOT [%1] == [] (
