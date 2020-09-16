2ec7c26a63061b7a51e7e2c52be238ffba3a234f
Ray Chen <tankimarshal2@gmail.com>
2020-09-16 20:52:00 +0100

    Changed 0.0.6 to 0.1.0

--- release.bat
+++ release.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-twine upload --repository testpypi dist/pyunity-0.0.6*
-twine upload dist/pyunity-0.0.6*
+twine upload --repository testpypi dist/pyunity-0.1.0*
+twine upload dist/pyunity-0.1.0*
