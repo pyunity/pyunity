b12ad21d9ac8053b5192f7f6e91da1b315e71360
Ray Chen <tankimarshal2@gmail.com>
2020-10-11 12:25:31 +0100

    Fixed glfwWindow errors

--- build.bat
+++ build.bat
@@ -3,9 +3,7 @@
 python prepare.py
 python setup.py build -c mingw32 bdist_wheel -d "dist/0.2.0/" sdist -d "dist/0.2.0/"
 RMDIR /S /Q "pyunity.egg-info/" "docs/build/html/"
-SET a=1
 py -3.7 setup.py build -c mingw32 bdist_wheel -d "dist/0.2.0/"
-SET a=
 RMDIR /S /Q "pyunity.egg-info/" "build/"
 sphinx-build -T -E -b html docs/source docs/build/html
 IF NOT [%1] == [] (
