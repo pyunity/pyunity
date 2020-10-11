1d62a8f9d63c22b06f3473a9300db4b29fe9c229
Ray Chen <tankimarshal2@gmail.com>
2020-10-11 19:37:00 +0100

    README generation

--- prepare.py
+++ prepare.py
@@ -1,7 +1,7 @@
 import os, glob, shutil, sys
 if "cython" not in os.environ: os.environ["cython"] = "1"
 
-if "a" not in os.environ:
+if len(sys.argv) == 1:
     import pyunity
     desc = pyunity.__doc__.split("\n")
     desc_new = [
