829d2aa18ece7c9a9a3e71678da93508ef75cc32
rayzchen <tankimarshal2@gmail.com>
2020-10-30 17:50:50 +0000

    Added releasing from release.bat [ci skip]

--- release.bat
+++ release.bat
@@ -1,3 +1,8 @@
 @ECHO OFF
+git checkout master
 twine upload --repository testpypi dist/0.2.0/pyunity-0.2.0*
 twine upload dist/0.2.0/pyunity-0.2.0*
+git checkout releases
+twine upload --repository testpypi 0.2.0/pyunity-0.2.0*
+twine upload 0.2.0/pyunity-0.2.0*
+git checkout master
