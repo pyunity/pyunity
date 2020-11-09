4dd11eccf5b8da582d15f7b9ba5d72d8a98a0554
Ray Chen <tankimarshal2@gmail.com>
2020-11-09 21:41:30 +0000

    Fixed sphinx warnings and added apidoc

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ prepare.py
 setup.py build -c mingw32 bdist_wheel -d "dist/0.3.0/" sdist -d "dist/0.3.0/"
 py -3.7 setup.py build -c mingw32 bdist_wheel -d "dist/0.3.0/"
 py -3.6 setup.py build -c mingw32 bdist_wheel -d "dist/0.3.0/"
-RMDIR /S /Q "pyunity.egg-info/" "build/" "docs/en/"
+RMDIR /S /Q "pyunity.egg-info/" "build/" "docs/en/" "docs/source/pyunity*"
+sphinx-apidoc -e -F -M -o docs/source pyunity pyunity/config.py pyunity/examples/*
 sphinx-build -T -E -b html docs/source docs/en
 IF NOT [%1] == [] (
 git add .
