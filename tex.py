c9a393a0d43adade6e97ea8ffba345aec3a856a5
Ray Chen <tankimarshal2@gmail.com>
2021-05-28 00:57:30 +0100

    ibo

--- tex.py
+++ tex.py
@@ -1,7 +1,7 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from PIL import Image
-from ctypes import c_float
+from ctypes import c_float, c_ubyte
 import pygame
 pygame.init()
 
@@ -30,13 +30,10 @@ vertices = [
     -1, -1, 0,
 ]
 
-vbo = glGenBuffers(1)
-vao = glGenVertexArrays(1)
-glBindVertexArray(vao)
-glBindBuffer(GL_ARRAY_BUFFER, vbo)
-glBufferData(GL_ARRAY_BUFFER, len(vertices) * 4, (c_float * len(vertices))(*vertices), GL_STATIC_DRAW)
-glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(c_float), None)
-glEnableVertexAttribArray(0)
+indices = [
+    0, 1, 2,
+    0, 2, 3,
+]
 
 vbo = glGenBuffers(1)
 vao = glGenVertexArrays(1)
@@ -45,6 +42,10 @@ glBindBuffer(GL_ARRAY_BUFFER, vbo)
 glBufferData(GL_ARRAY_BUFFER, len(vertices) * 4, (c_float * len(vertices))(*vertices), GL_STATIC_DRAW)
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(c_float), None)
 glEnableVertexAttribArray(0)
+
+ibo = glGenBuffers(1)
+glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo)
+glBufferData(GL_ELEMENT_ARRAY_BUFFER, len(indices), (c_ubyte * len(indices))(*indices), GL_STATIC_DRAW)
 
 gluPerspective(96, 800 / 500, 0.03, 50)
 glTranslatef(0, 0, -3)
@@ -68,7 +69,9 @@ while not done:
     # glTexCoord2f(0, 1)
     # glVertex3f(-1, -1, 0)
     # glEnd()
-    glDrawArrays(GL_QUADS, 0, 4)
+
+    # glDrawArrays(GL_QUADS, 0, 4)
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, None)
 
     pygame.display.flip()
     clock.tick(60)
