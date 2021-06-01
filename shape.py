ff6f5c3c9da7fe777f8205b51a681d4a0d713bf2
Ray Chen <tankimarshal2@gmail.com>
2021-06-01 18:00:42 +0100

    Sphere

--- shape.py
+++ shape.py
@@ -1,7 +1,7 @@
 from pyunity import *
 import math
 
-def cylinder(detail):
+def gen_cylinder(detail):
     points = []
 
     for i in range(detail):
@@ -63,12 +63,43 @@ def cylinder(detail):
 
     return Mesh(final_points, final_indices, final_normals, final_texcoords)
 
+def gen_sphere(detail):
+    t = (1 + math.sqrt(5)) / 2
+    points1 = [Vector3(a, b, 0).normalized() for b in (t, -t) for a in (-1, 1)]
+    points2 = [Vector3(0, a, b).normalized() for b in (t, -t) for a in (-1, 1)]
+    points3 = [Vector3(b, 0, a).normalized() for b in (t, -t) for a in (-1, 1)]
+    points = points1 + points2 + points3
+
+    indices = [
+        [0, 11, 5], [0, 5, 1], [0, 1, 7], [0, 7, 10], [0, 10, 11],
+        [1, 5, 9], [5, 11, 4], [11, 10, 2], [10, 7, 6], [7, 1, 8],
+        [3, 9, 4], [3, 4, 2], [3, 2, 6], [3, 6, 8], [3, 8, 9],
+        [4, 9, 5], [2, 4, 11], [6, 2, 10], [8, 6, 7], [9, 8, 1],
+    ]
+
+    for i in range(detail):
+        indices2 = []
+        for tri in indices:
+            points.append(((points[tri[0]] + points[tri[1]]) / 2).normalized())
+            points.append(((points[tri[1]] + points[tri[2]]) / 2).normalized())
+            points.append(((points[tri[2]] + points[tri[0]]) / 2).normalized())
+            indices2.append([tri[0], len(points) - 3, len(points) - 1])
+            indices2.append([tri[1], len(points) - 2, len(points) - 3])
+            indices2.append([tri[2], len(points) - 1, len(points) - 2])
+            indices2.append([len(points) - 3, len(points) - 2, len(points) - 1])
+        indices = indices2.copy()
+
+    normals = points.copy()
+    return Mesh(points, indices, normals, None)
+
 class Rotator(Behaviour):
     def Update(self, dt):
         self.transform.eulerAngles += Vector3(0, 90, 0) * dt
 
-Loader.SaveMesh(cylinder(60), "cylinder")
-Loader.SaveObj(cylinder(60), "cylinder")
+Loader.SaveMesh(gen_cylinder(60), "cylinder")
+Loader.SaveObj(gen_cylinder(60), "cylinder")
+Loader.SaveMesh(gen_sphere(2), "sphere")
+Loader.SaveObj(gen_sphere(2), "sphere")
 
 scene = SceneManager.AddScene("Scene")
 
@@ -77,11 +108,19 @@ scene.mainCamera.transform.eulerAngles = Vector3(15, 0, 0)
 # scene.mainCamera.transform.localPosition = Vector3(0, 10, 0)
 # scene.mainCamera.transform.eulerAngles = Vector3(90, 0, 0)
 
-mesh = Loader.LoadMesh("cylinder.mesh")
-cylinder = GameObject("Cylinder")
-cylinder.AddComponent(Rotator)
-renderer = cylinder.AddComponent(MeshRenderer)
-renderer.mesh = mesh
+# mesh = Loader.LoadMesh("cylinder.mesh")
+# cylinder = GameObject("Cylinder")
+# cylinder.AddComponent(Rotator)
+# renderer = cylinder.AddComponent(MeshRenderer)
+# renderer.mesh = mesh
+# renderer.mat = Material(Color(255, 255, 255), Texture2D("..\\pyunity.png"))
+# scene.Add(cylinder)
+
+mesh = Loader.LoadMesh("sphere.mesh")
+sphere = GameObject("sphere")
+sphere.AddComponent(Rotator)
+renderer = sphere.AddComponent(MeshRenderer)
+renderer.mesh = gen_sphere(2)
 renderer.mat = Material(Color(255, 255, 255), Texture2D("..\\pyunity.png"))
-scene.Add(cylinder)
+scene.Add(sphere)
 SceneManager.LoadScene(scene)
