45cafb3a711b3fb110318dfc60a3eac1f21c38a6
Ray Chen <tankimarshal2@gmail.com>
2021-06-03 21:59:35 +0100

    Fixed sphere

--- gltests/shape.py
+++ gltests/shape.py
@@ -1,4 +1,6 @@
 from pyunity import *
+# from pyunity import config
+# config.faceCulling = False
 import math
 
 def gen_cylinder(detail):
@@ -67,33 +69,31 @@ def gen_sphere(detail):
     def texcoord(vec):
         return [math.atan2(vec.x, vec.z) / math.pi, -vec.y / 2 - 0.5]
 
-    t = (1 + math.sqrt(5)) / 2
-    points1 = [Vector3(a, b, 0).normalized() for b in (t, -t) for a in (-1, 1)]
-    points2 = [Vector3(0, a, b).normalized() for b in (t, -t) for a in (-1, 1)]
-    points3 = [Vector3(b, 0, a).normalized() for b in (t, -t) for a in (-1, 1)]
-    points = points1 + points2 + points3
-
-    indices = [
-        [0, 11, 5], [0, 5, 1], [0, 1, 7], [0, 7, 10], [0, 10, 11],
-        [1, 5, 9], [5, 11, 4], [11, 10, 2], [10, 7, 6], [7, 1, 8],
-        [3, 9, 4], [3, 4, 2], [3, 2, 6], [3, 6, 8], [3, 8, 9],
-        [4, 9, 5], [2, 4, 11], [6, 2, 10], [8, 6, 7], [9, 8, 1],
-    ]
-
-    for i in range(detail):
-        indices2 = []
-        for tri in indices:
-            points.append(((points[tri[0]] + points[tri[1]]) / 2).normalized())
-            points.append(((points[tri[1]] + points[tri[2]]) / 2).normalized())
-            points.append(((points[tri[2]] + points[tri[0]]) / 2).normalized())
-            indices2.append([tri[0], len(points) - 3, len(points) - 1])
-            indices2.append([tri[1], len(points) - 2, len(points) - 3])
-            indices2.append([tri[2], len(points) - 1, len(points) - 2])
-            indices2.append([len(points) - 3, len(points) - 2, len(points) - 1])
-        indices = indices2.copy()
+    points = []
+    indices = []
+    texcoords = []
+    for i in range(detail // 2 + 1):
+        sin1 = math.sin(i / detail * math.pi * 2)
+        cos1 = math.cos(i / detail * math.pi * 2)
+        for j in range(detail):
+            base = i * (detail + 1) + j
+            base2 = (i + 1) * (detail + 1) + j
+            sin2 = math.sin(j / detail * math.pi * 2)
+            cos2 = math.cos(j / detail * math.pi * 2)
+            points.append(Vector3(sin2 * cos1, cos2, sin1 * sin2))
+            texcoords.append([i / detail * 2, -cos2 / 2 + 0.5])
+            if j > detail // 2:
+                indices.append([base, base + 1, base2 + 1])
+                indices.append([base, base2 + 1, base2])
+            else:
+                indices.append([base, base2 + 1, base + 1])
+                indices.append([base, base2, base2 + 1])
+        points.append(Vector3(0, 1, 0))
+        texcoords.append([i / detail * 2, 1])
+    del indices[-2 * detail:]
 
     normals = points.copy()
-    texcoords = list(map(texcoord, points))
+    # texcoords = list(map(texcoord, points))
     return Mesh(points, indices, normals, texcoords)
 
 class Rotator(Behaviour):
@@ -102,8 +102,8 @@ class Rotator(Behaviour):
 
 Loader.SaveMesh(gen_cylinder(60), "cylinder")
 Loader.SaveObj(gen_cylinder(60), "cylinder")
-Loader.SaveMesh(gen_sphere(4), "sphere")
-Loader.SaveObj(gen_sphere(4), "sphere")
+Loader.SaveMesh(gen_sphere(20), "sphere")
+Loader.SaveObj(gen_sphere(20), "sphere")
 
 scene = SceneManager.AddScene("Scene")
 
@@ -116,7 +116,7 @@ scene.mainCamera.transform.localPosition = Vector3(0, 0, -7.5)
 # cylinder.AddComponent(Rotator)
 # renderer = cylinder.AddComponent(MeshRenderer)
 # renderer.mesh = mesh
-# renderer.mat = Material(Color(255, 255, 255), Texture2D("..\\pyunity.png"))
+# renderer.mat = Material(Color(255, 255, 255), Texture2D("..\\..\\pyunity.png"))
 # scene.Add(cylinder)
 
 mesh = Loader.LoadMesh("sphere.mesh")
