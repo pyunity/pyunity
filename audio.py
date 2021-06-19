f1b7a025d6f2ad9b7cbd27a3d8e7fb5731468fb7
Ray Chen <tankimarshal2@gmail.com>
2021-06-19 20:46:36 +0100

    Check for finished channels

--- audio.py
+++ audio.py
@@ -20,16 +20,18 @@ class AudioSource:
     
     def Play(self):
         def finished(channel):
+            print("Channel %d finished playing" % channel)
             self.playing = False
         
         self.playing = True
         self.func = ctypes.CFUNCTYPE(None, ctypes.c_int)(finished)
         if Mix_PlayChannel(self.channel, self.clip.music, 0) == -1:
-            print("Unable to play Ogg file: %s" % Mix_GetError())
+            print("Unable to play file: %s" % Mix_GetError())
         Mix_ChannelFinished(self.func)
     
     def Stop(self):
         Mix_HaltChannel(self.channel)
+        self.playing = False
     
     def Pause(self):
         Mix_Pause(self.channel)
@@ -55,12 +57,13 @@ class AudioListener:
         self.sources.append(source)
     
     def wait_for_sources(self):
-        finished = 0
-        while finished != len(self.sources):
-            finished = 0
-            for source in self.sources:
-                if source.playing == False:
-                    finished += 1
+        while True:
+            playing = 0
+            for i in range(channels):
+                if Mix_Playing(i):
+                    playing += 1
+            if playing == 0:
+                break
     
     def deinit(self):
         for source in self.sources:
@@ -69,9 +72,10 @@ class AudioListener:
         Mix_CloseAudio()
 
 listener = AudioListener()
-clip = AudioClip("pyunity/examples/example8/explode.ogg")
-source = AudioSource(clip)
-listener.add_source(source)
-source.Play()
+for i in range(16):
+    clip = AudioClip("pyunity/examples/example8/explode.ogg")
+    source = AudioSource(clip)
+    listener.add_source(source)
+    source.Play()
 listener.wait_for_sources()
 listener.deinit()
