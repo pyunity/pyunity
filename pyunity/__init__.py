83b1989727126c7260f0ac829348d12a2e4c0acb
Ray Chen <tankimarshal2@gmail.com>
2021-06-21 11:42:56 +0100

    Removed references to key

--- pyunity/__init__.py
+++ pyunity/__init__.py
@@ -167,7 +167,7 @@ __uri__ = "https://pyunity.readthedocs.io/en/latest/"
 from . import audio, core, physics, errors, files
 __all__ = ["__version__", "Vector3", "Quaternion",
            "SceneManager", "Mesh", "Loader",
-           "KeyCode", "Logger", "Behaviour"]
+           "Logger", "Behaviour"]
 __all__.extend(audio.__all__)
 __all__.extend(core.__all__)
 __all__.extend(physics.__all__)
