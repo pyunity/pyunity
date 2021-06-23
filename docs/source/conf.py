9ab6eee12380c89a072da1ef9442244750bb044d
Ray Chen <tankimarshal2@gmail.com>
2021-06-23 10:21:40 +0100

    Rename back to 0.6.0 [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020-2021, Ray Chen'
 author = 'Ray Chen'
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.2"
+release = "0.6.0"
 
 
 # -- General configuration ---------------------------------------------------
