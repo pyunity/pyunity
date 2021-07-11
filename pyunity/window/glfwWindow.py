e6c5991b9f1cb8cab2bcc5dcd245c22e0af28bdf
Ray Chen <tankimarshal2@gmail.com>
2021-07-11 13:31:11 +0100

    pep8

--- pyunity/window/glfwWindow.py
+++ pyunity/window/glfwWindow.py
@@ -25,7 +25,8 @@ class Window:
             glfw.window_hint(glfw.GLFW_CONTEXT_VERSION_MAJOR, 3)
             glfw.window_hint(glfw.GLFW_CONTEXT_VERSION_MINOR, 3)
             glfw.window_hint(glfw.GLFW_OPENGL_FORWARD_COMPAT, glfw.GL_TRUE)
-            glfw.window_hint(glfw.GLFW_OPENGL_PROFILE, glfw.GLFW_OPENGL_CORE_PROFILE)
+            glfw.window_hint(glfw.GLFW_OPENGL_PROFILE,
+                             glfw.GLFW_OPENGL_CORE_PROFILE)
 
         self.window = glfw.create_window(*config.size, name, None, None)
         if not self.window:
