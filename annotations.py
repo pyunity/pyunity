6972cb7e45e41a71046b1a4e02953f3f57b60921
Ray Chen <tankimarshal2@gmail.com>
2021-07-17 20:08:34 +0100

    Detect missing stub contents

--- annotations.py
+++ annotations.py
@@ -2,51 +2,75 @@ import os
 import glob
 import re
 
-os.chdir("pyunity")
-files = glob.glob("**/*.py", recursive=True)
-files = list(filter(lambda x: not x.startswith("examples"), files))
+def check_folder(folder, ext):
+    os.chdir(folder)
+    files = glob.glob("**/*." + ext, recursive=True)
+    files = list(filter(lambda x: not x.startswith("examples"), files))
 
-class_find = re.compile("(?<=^class )(\\w+)(?=(\(.*?\))?:)")
-method_find = re.compile("(?<=    def )(\\w+)")
-func_find = re.compile("(?<=^def )(\\w+)")
-attr_find = re.compile("([a-zA-Z]\w+)(?= ?= ?)")
+    class_find = re.compile("(?<=^class )(\\w+)(?=(\(.*?\))?:)")
+    method_find = re.compile("(?<=    def )(\\w+)")
+    func_find = re.compile("(?<=^def )(\\w+)")
+    attr_find = re.compile("([a-zA-Z]\w+)(?= ?= ?)")
 
+    classes = []
+    functions = []
+    attrs = []
+
+    for file in files:
+        with open(file, "r") as f:
+            content = f.read().rstrip().splitlines()
+        
+        module = "pyunity." + os.path.splitext(file)[0].replace(os.path.sep, ".")
+        module = module.replace(".__init__", "") + "."
+        current_class = {}
+        for line in content:
+            if re.search(class_find, line):
+                if current_class != {}:
+                    current_class["name"] = current_class["name"]
+                    classes.append(current_class)
+                current_class = {"name": module + re.search(class_find, line).group(),
+                                "methods": []}
+            elif re.search(method_find, line):
+                current_class["methods"].append(module + current_class["name"] + \
+                    "." + re.search(method_find, line).group())
+            elif re.search(func_find, line):
+                functions.append(module + re.search(func_find, line).group())
+            elif re.match(attr_find, line):
+                attrs.append(module + re.match(attr_find, line).group())
+        
+        if current_class != {}:
+            classes.append(current_class)
+    os.chdir("..")
+    return classes, functions, attrs
+
+a, b, c = check_folder("pyunity", "py")
+d, e, f = check_folder("stubs", "pyi")
 classes = []
-functions = []
-attrs = []
+for class_ in a:
+    for class2 in d:
+        if class2["name"] == class_["name"]:
+            break
+    else:
+        continue
 
-for file in files:
-    with open(file, "r") as f:
-        content = f.read().rstrip().splitlines()
-    
-    module = "pyunity." + os.path.splitext(file)[0].replace(os.path.sep, ".")
-    module = module.replace(".__init__", "") + "."
-    current_class = {}
-    for line in content:
-        if re.search(class_find, line):
-            if current_class != {}:
-                current_class["name"] = current_class["name"]
-                classes.append(current_class)
-            current_class = {"name": module + re.search(class_find, line).group(),
-                            "methods": []}
-        elif re.search(method_find, line):
-            current_class["methods"].append(module + current_class["name"] + \
-                "." + re.search(method_find, line).group())
-        elif re.search(func_find, line):
-            functions.append(module + re.search(func_find, line).group())
-        elif re.match(attr_find, line):
-            attrs.append(module + re.match(attr_find, line).group())
-    
-    if current_class != {}:
-        classes.append(current_class)
+    if class_["methods"] == []:
+        continue
+    current = {"name": class_["name"]}
+    current["methods"] = [item for item in class_["methods"] if item not in class2["methods"]]
+    if current["methods"] == []:
+        continue
+    classes.append(current)
+
+functions = [item for item in b if item not in e]
+attrs = [item for item in c if item not in f]
 
 for class_ in classes:
     print("Class missing: " + class_["name"])
     for method in class_["methods"]:
         print("    Method missing: " + method)
-print()
-for func in functions:
-    print("Function missing: " + func)
-print()
-for attr in attrs:
-    print("Variable missing: " + attr)
+# print()
+# for func in functions:
+#     print("Function missing: " + func)
+# print()
+# for attr in attrs:
+#     print("Variable missing: " + attr)
