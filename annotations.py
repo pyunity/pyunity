35c62627222d97578cabd59fa6896891352300db
Ray Chen <tankimarshal2@gmail.com>
2021-07-17 19:44:24 +0100

    Scan folder for funcs and classes

--- annotations.py
+++ annotations.py
@@ -1,23 +1,52 @@
-import pyunity
-import inspect
+import os
+import glob
+import re
 
-f = open("missing_annotations.txt", "w+")
+os.chdir("pyunity")
+files = glob.glob("**/*.py", recursive=True)
+files = list(filter(lambda x: not x.startswith("examples"), files))
 
-for item in pyunity.__all__:
-    obj = getattr(pyunity, item)
-    if not isinstance(obj, type):
-        continue
-    annotations = getattr(obj, "__annotations__", {})
-    functions = [name for name, value in vars(obj).items() if inspect.isroutine(value)]
-    attributes = [name for name, value in vars(obj).items() if not (inspect.isroutine(value) or name.startswith("__"))]
+class_find = re.compile("(?<=^class )(\\w+)(?=(\(.*?\))?:)")
+method_find = re.compile("(?<=    def )(\\w+)")
+func_find = re.compile("(?<=^def )(\\w+)")
+attr_find = re.compile("([a-zA-Z]\w+)(?= ?= ?)")
 
-    for function in functions:
-        if function not in annotations:
-            print(item + "." + function + " is not annotated (FUNCTION)")
-            f.write(item + "." + function + " is not annotated (FUNCTION)\n")
-    for attribute in attributes:
-        if attribute not in annotations:
-            print(item + "." + attribute + " is not annotated (ATTRIBUTE)")
-            f.write(item + "." + attribute + " is not annotated (ATTRIBUTE)\n")
+classes = []
+functions = []
+attrs = []
 
-f.close()
+for file in files:
+    with open(file, "r") as f:
+        content = f.read().rstrip().splitlines()
+    
+    module = "pyunity." + os.path.splitext(file)[0].replace(os.path.sep, ".")
+    module = module.replace(".__init__", "") + "."
+    current_class = {}
+    for line in content:
+        if re.search(class_find, line):
+            if current_class != {}:
+                current_class["name"] = current_class["name"]
+                classes.append(current_class)
+            current_class = {"name": module + re.search(class_find, line).group(),
+                            "methods": []}
+        elif re.search(method_find, line):
+            current_class["methods"].append(module + current_class["name"] + \
+                "." + re.search(method_find, line).group())
+        elif re.search(func_find, line):
+            functions.append(module + re.search(func_find, line).group())
+        elif re.match(attr_find, line):
+            attrs.append(module + re.match(attr_find, line).group())
+    
+    if current_class != {}:
+        classes.append(current_class)
+
+for class_ in classes:
+    print("Class missing: " + class_["name"])
+    for method in class_["methods"]:
+        print("    Method missing: " + method)
+print()
+for func in functions:
+    print("Function missing: " + func)
+print()
+for attr in attrs:
+    print("Variable missing: " + attr)
