e1bd60190dd8c534c4b63950385f1b4824f6842e
Ray Chen <tankimarshal2@gmail.com>
2021-07-31 10:10:51 +0100

    DefaultMaterial for ball and wall [ci skip]

--- ball.py
+++ ball.py
@@ -30,8 +30,7 @@ rb.mass = 50
 rb.gravity = False
 rb.physicMaterial = PhysicMaterial(0.5, 0)
 renderer = ball.AddComponent(MeshRenderer)
-renderer.mesh = Loader.Primitives.sphere
-renderer.mat = Material(RGB(200, 200, 200))
+renderer.mesh = Loader.Primitives.sphereerial(RGB(200, 200, 200))
 ball.AddComponent(PlayerController).rigidbody = rb
 scene.Add(ball)
 
@@ -43,11 +42,8 @@ floor.transform.position = Vector3(0, -2, 0)
 floor.transform.scale = Vector3(50, 1, 50)
 floor.AddComponent(AABBoxCollider)
 renderer = floor.AddComponent(MeshRenderer)
-renderer.mesh = Loader.Primitives.cube
-renderer.mat = Material(RGB(50, 50, 50))
+renderer.mesh = Loader.Primitives.cubeerial(RGB(50, 50, 50))
 scene.Add(floor)
-
-mat = Material(RGB(200, 200, 200))
 
 wall1 = GameObject("Wall")
 wall1.transform.position = Vector3(0, 0.5, 51)
@@ -55,7 +51,6 @@ wall1.transform.scale = Vector3(50, 3, 1)
 wall1.AddComponent(AABBoxCollider)
 renderer = wall1.AddComponent(MeshRenderer)
 renderer.mesh = Loader.Primitives.cube
-renderer.mat = mat
 scene.Add(wall1)
 
 wall2 = GameObject("Wall")
@@ -64,7 +59,6 @@ wall2.transform.scale = Vector3(50, 3, 1)
 wall2.AddComponent(AABBoxCollider)
 renderer = wall2.AddComponent(MeshRenderer)
 renderer.mesh = Loader.Primitives.cube
-renderer.mat = mat
 scene.Add(wall2)
 
 wall3 = GameObject("Wall")
@@ -73,7 +67,6 @@ wall3.transform.scale = Vector3(1, 3, 50)
 wall3.AddComponent(AABBoxCollider)
 renderer = wall3.AddComponent(MeshRenderer)
 renderer.mesh = Loader.Primitives.cube
-renderer.mat = mat
 scene.Add(wall3)
 
 wall4 = GameObject("Wall")
@@ -82,9 +75,8 @@ wall4.transform.scale = Vector3(1, 3, 50)
 wall4.AddComponent(AABBoxCollider)
 renderer = wall4.AddComponent(MeshRenderer)
 renderer.mesh = Loader.Primitives.cube
-renderer.mat = mat
 scene.Add(wall4)
 
-# SceneManager.LoadSceneByIndex(0)
+SceneManager.LoadSceneByIndex(0)
 
 Loader.SaveSceneToProject(scene, name="Roll A Ball")
