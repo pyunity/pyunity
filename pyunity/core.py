7a030cdd0fe27f475464edf0cfa08db750fed48d
Ray Chen <tankimarshal2@gmail.com>
2021-08-10 09:48:38 +0100

    Fixed core.__all__

--- pyunity/core.py
+++ pyunity/core.py
@@ -46,7 +46,7 @@ and all have MeshRenderers:
 
 """
 
-__all__ = ["Component", "GameObject", "Light", "Clock",
+__all__ = ["Component", "GameObject", "Light",
            "MeshRenderer", "Tag", "Transform", "ShowInInspector"]
 
 import glm
