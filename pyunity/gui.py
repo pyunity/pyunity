ce6a33e2a4ed9fa76e4466494cde297f24446635
Ray Chen <tankimarshal2@gmail.com>
2021-08-18 21:52:30 +0100

    Start RectTransform

--- pyunity/gui.py
+++ pyunity/gui.py
@@ -1,9 +1,22 @@
-from .core import Component, SingleComponent
+from .values import Vector2
+from .core import Component, ShowInInspector, Transform
 
 class Canvas(Component):
     def Render(self):
         for descendant in self.transform.GetDescendants():
             print(descendant.gameObject.name)
 
-class RectTransform(SingleComponent):
-    pass
+class RectAnchors:
+    def __init__(self):
+        self.min = Vector2(0.5, 0.5)
+        self.max = Vector2(0.5, 0.5)
+
+class RectTransform(Transform):
+    anchoredPos = ShowInInspector(Vector2, None, "position")
+    anchors = ShowInInspector(RectAnchors, None)
+    pivot = ShowInInspector(Vector2, None)
+    def __init__(self, transform=None):
+        super(RectTransform, self).__init__(transform)
+        self.anchoredPos = Vector2.zero()
+        self.anchors = RectAnchors()
+        self.pivot = Vector2(0.5, 0.5)
