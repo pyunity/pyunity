b592f57933b4cad2e53345cdf00493669093c26b
Ray Chen <tankimarshal2@gmail.com>
2021-08-18 18:12:06 +0100

    Vector2 and fix Vector3

--- pyunity/values/__init__.py
+++ pyunity/values/__init__.py
@@ -1,8 +1,9 @@
 from . import texture
-__all__ = ["Clock", "Quaternion", "Vector3"]
+__all__ = ["Clock", "Quaternion", "Vector2", "Vector3"]
 __all__.extend(texture.__all__)
 
 from .other import Clock
 from .quaternion import Quaternion
 from .texture import *
+from .vector2 import Vector2
 from .vector3 import Vector3
