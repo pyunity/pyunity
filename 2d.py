a907ad6e8025d1065f259a9bd0857e6b11bc28fc
Ray Chen <tankimarshal2@gmail.com>
2021-08-25 17:47:31 +0100

    Use scale

--- 2d.py
+++ 2d.py
@@ -8,6 +8,7 @@ class Rotator2D(Behaviour):
 scene = SceneManager.AddScene("Scene")
 imgObject = GameObject("Image")
 rectTransform = imgObject.AddComponent(RectTransform)
+rectTransform.scale = Vector2(100, 100)
 img = imgObject.AddComponent(Image2D)
 img.texture = Texture2D("pyunity/examples/example9/logo.png")
 imgObject.AddComponent(Rotator2D).rectTransform = rectTransform
