fdb6696d1f1f0c66979e4c240da587124280086a
Ray Chen <tankimarshal2@gmail.com>
2021-09-03 21:25:21 +0100

    Fix build script

--- docs/build.py
+++ docs/build.py
@@ -4,16 +4,20 @@ import os
 
 subprocess.call(["sphinx-apidoc", "-e", "-f", "-T", "-o",
                 "docs/source/api/", "pyunity", "pyunity/config.py", "pyunity/examples/"],
-                env={"SPHINX_APIDOC_OPTIONS": "members,inherited-members,show-inheritance"})
+                env={**os.environ, "SPHINX_APIDOC_OPTIONS": "members,inherited-members,show-inheritance"})
 
-os.remove("docs/source/api/modules.rst")
 with open("docs/source/api/pyunity.rst") as f:
     lines = f.read().splitlines()[:-8]
 
 lines[0] = "API Documentation"
 lines[1] = "================="
-lines.insert(2, "")
 lines.insert(2, "Information on specific functions, classes, and methods.")
+lines.insert(2, "")
+for i in range(len(lines)):
+    if lines[i].startswith("   pyunity"):
+        lines[i] = "   api/" + lines[i][3:]
+
+lines.append("")
 
 with open("docs/source/api.rst", "w") as f:
     f.write("\n".join(lines))
