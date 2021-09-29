2faaf97acd18728bc13edbdae2582b4f3ffbcd64
Ray Chen <tankimarshal2@gmail.com>
2021-09-29 21:46:13 +0100

    `__all__` checker

--- pyunity/audio.py
+++ pyunity/audio.py
@@ -99,7 +99,6 @@ class AudioSource(Component):
         self.clip = None
         self.channel = channels
         channels += 1
-        print(channels)
         mixer.Mix_AllocateChannels(channels)
 
     def SetClip(self, clip):
