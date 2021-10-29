4eeeb18c493f1183a3dcdff8309b8526f7e2d197
Ray Chen <tankimarshal2@gmail.com>
2021-10-29 12:15:35 +0100

    Convert stubs to __future__ imports

---
+++ stubs/import.py
@@ -0,0 +1,25 @@
+import os
+import glob
+import shutil
+
+orig = os.path.dirname(os.path.abspath(__file__))
+os.chdir(orig)
+
+if os.path.isdir("copied"):
+    shutil.rmtree("copied")
+
+for file in glob.glob("pyunity/**/*.pyi", recursive=True):
+    new = os.path.join("copied", file[:-1])
+    os.makedirs(os.path.dirname(new), exist_ok=True)
+    print(new)
+    shutil.copy(file, new)
+    with open(new) as f:
+        content = f.read()
+    
+    if content.startswith('"""'):
+        new_content = content.replace('"""\n\n','"""\n\nfrom __future__ import annotations\n\n')
+    else:
+        new_content = "from __future__ import annotations\n\n" + content
+
+    with open(new, "w") as f:
+        f.write(new_content)
