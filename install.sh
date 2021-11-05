3c8d1f281764e720c12cf387afea492cba0c4080
Hunter2807 <46440327+Hunter2807@users.noreply.github.com>
2021-11-05 21:54:39 +0530

    Create install.sh
    Run the install.sh file now to install the module!

---
+++ install.sh
@@ -0,0 +1,3 @@
+python setup.py install #Run the setup
+
+pause #So that the command prompt does not exit till the user doesn't press any key... (Useful in case there are any errors while installing)
