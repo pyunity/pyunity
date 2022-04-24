22882e5bf3d777a909b0420fa5b5270ad9768d41
Ray Chen <tankimarshal2@gmail.com>
2022-04-24 11:49:57 +0100

    Use requirements.txt if found

--- pyunity/__main__.py
+++ pyunity/__main__.py
@@ -3,43 +3,58 @@
 # See https://docs.pyunity.x10.bz/en/latest/license.html
 
 import re
+import os
 import sys
 import platform
 import pkgutil
 from ._version import __version__
+from . import Logger
 
 TITLE_WIDTH = 30
 
 if "--version" in sys.argv or "-v" in sys.argv:
-    print("#" * TITLE_WIDTH)
-    print("VERSION INFO".center(TITLE_WIDTH))
-    print("#" * TITLE_WIDTH)
+    Logger.Log("#" * TITLE_WIDTH)
+    Logger.Log("VERSION INFO".center(TITLE_WIDTH))
+    Logger.Log("#" * TITLE_WIDTH)
 
     vstr = "v{0.major}.{0.minor}.{0.micro}-{0.releaselevel}"
-    print("PyUnity version: v" + __version__)
-    print("Python version:", vstr.format(sys.version_info))
-    print("Operating system:", platform.system(), platform.release())
-    print("Machine:", platform.machine())
-    print("Python architecture:", platform.architecture()[0])
+    Logger.Log("PyUnity version: v" + __version__)
+    Logger.Log("Python version:", vstr.format(sys.version_info))
+    Logger.Log("Operating system:", platform.system(), platform.release())
+    Logger.Log("Machine:", platform.machine())
+    Logger.Log("Python architecture:", platform.architecture()[0])
 
     if sys.version_info >= (3, 8):
         from importlib.metadata import requires, version, PackageNotFoundError
     elif pkgutil.find_loader("importlib_metadata") is not None:
         from importlib_metadata import requires, version, PackageNotFoundError
     else:
-        requires = None
+        Logger.LogLine(Logger.WARN,
+            "Python version less than 3.8 but no importlib_metadata found")
 
     if requires is not None:
-        print("Requirements:")
+        Logger.Log("Dependencies:")
+        try:
+            requirements = requires("pyunity")
+        except PackageNotFoundError:
+            Logger.LogLine(Logger.WARN,
+                "PyUnity not ran as an installed package")
+            requirements = []
+        if not len(requirements):
+            if os.path.isfile("requirements.txt"):
+                with open("requirements.txt") as f:
+                    requirements = f.read().rstrip().split("\n")
+            else:
+                Logger.LogLine(Logger.WARN,
+                    "No requirements.txt file found")
+
         for item in requires("pyunity"):
             name = re.split(" |;", item)[0]
             try:
-                print("-", name, "version:", version(name))
+                Logger.Log("-", name, "version:", version(name))
             except PackageNotFoundError:
                 # python_version or sys.platform used
-                print("-", name, "version:", None)
-    else:
-        print("Python version less than 3.8 but no importlib_metadata found")
+                Logger.Log("-", name, "version:", None)
 else:
     from . import examples
     examples.show()
