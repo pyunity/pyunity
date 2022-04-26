30433be0938ccfccda4b3ff51ddcbd7122da4e3e
Ray Chen <tankimarshal2@gmail.com>
2022-04-26 17:34:35 +0100

    Color tests

---
+++ tests/pyunity/testValues/testColor.py
@@ -0,0 +1,29 @@
+from pyunity import Color, RGB, HSV
+from . import TestCase
+
+class TestColors(TestCase):
+    def testFromString(self):
+        a = Color.fromString("RGB(127, 0, 255)")
+        assert a.r == 127
+        assert a.g == 0
+        assert a.b == 255
+        assert a.toString() == "RGB(127, 0, 255)"
+
+        b = Color.fromString("HSV(180, 50, 75)")
+        assert b.h == 180
+        assert b.s == 50
+        assert b.v == 75
+        assert b.toString() == "HSV(180, 50, 75)"
+
+    def testConvert(self):
+        a = RGB(127, 0, 255)
+        assert a.toRGB() is a
+
+        b = HSV(180, 50, 75)
+        assert b.toHSV() is b
+
+class TestRGB(TestCase):
+    def testDivMul(self):
+        a = RGB(127, 0, 255)
+        assert a / 255 == (127/255, 0, 1.0)
+        assert a * 2 == (254, 0, 510)
