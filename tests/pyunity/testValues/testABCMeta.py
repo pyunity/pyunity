101da86b6370b4e8d7c8bc70faba1d4416375299
Ray Chen <tankimarshal2@gmail.com>
2022-04-26 17:02:03 +0100

    ABC tests

---
+++ tests/pyunity/testValues/testABCMeta.py
@@ -0,0 +1,67 @@
+from pyunity import ABCMeta, abstractmethod, abstractproperty, ABCException, ABCMessage
+from . import TestCase
+
+class TestAbstractmethod(TestCase):
+    def testInit(self):
+        class ABCTest(metaclass=ABCMeta):
+            @abstractmethod
+            def meth(self):
+                pass
+
+        assert ABCTest.meth.__name__ == "meth"
+
+        with self.assertRaises(ABCException) as exc:
+            abstractmethod(1)
+        assert exc.value == "Function is not callable: 1"
+
+class TestAbstractproperty(TestCase):
+    def testInit(self):
+        class ABCTest(metaclass=ABCMeta):
+            @abstractproperty
+            def prop(self):
+                pass
+
+        assert ABCTest.prop.__name__ == "prop"
+        assert ABCTest().prop is None
+
+class ABCTest(metaclass=ABCMeta):
+    @abstractmethod
+    def meth(self):
+        pass
+
+    @abstractproperty
+    def prop(self):
+        pass
+
+class TestABCMeta(TestCase):
+    def testCreate(self):
+        class ABCSub(ABCTest):
+            def meth(self):
+                return 5
+
+            @property
+            def prop(self):
+                return 2
+
+        assert ABCSub is not None
+
+    def testError(self):
+        with self.assertRaises(ABCException) as exc:
+            class ABCSub(ABCTest):
+                def meth(self):
+                    return 5
+            ABCSub().meth()
+        assert exc.value.startswith("Method has not been defined: ")
+
+        with self.assertRaises(ABCException) as exc:
+            class ABCSub(ABCTest):
+                def meth(self, a):
+                    return 5
+            ABCSub().meth(1)
+        assert exc.value.startswith("Function signature is not the same: ")
+
+        with self.assertRaises(ABCMessage) as exc:
+            class ABCSub(ABCTest, message="error message"):
+                def meth(self):
+                    return 5
+        assert exc.value == "error message"
