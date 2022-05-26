8f3bfbec9cfdc4dfef46b923b9d4bee07a5164f8
Ray Chen <54078384+rayzchen@users.noreply.github.com>
2022-05-26 19:49:24 +0000

    More env var documentation

--- pyunity/config.py
+++ pyunity/config.py
@@ -4,12 +4,18 @@
 
 import os
 from . import Logger
+
 if "PYUNITY_DEBUG_MODE" not in os.environ:
     os.environ["PYUNITY_DEBUG_MODE"] = "1"
-if "PYUNITY_INTERACTIVE" not in os.environ:
-    os.environ["PYUNITY_INTERACTIVE"] = "1"
 if "PYUNITY_AUDIO" not in os.environ:
     os.environ["PYUNITY_AUDIO"] = "1"
+if "PYUNITY_CHECK_WINDOW" not in os.environ:
+    os.environ["PYUNITY_CHECK_WINDOW"] = "0"
+if "PYUNITY_INTERACTIVE" not in os.environ:
+    os.environ["PYUNITY_INTERACTIVE"] = "1"
+if "PYUNITY_CHANGE_MODULE" not in os.environ:
+    os.environ["PYUNITY_CHANGE_MODULE"] = "1"
+
 os.environ["MESA_GL_VERSION_OVERRIDE"] = "3.3"
 os.environ["MESA_GLSL_VERSION_OVERRIDE"] = "330"
 
