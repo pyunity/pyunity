b5787bb5fa05d5639234142cdcc78a44b5a8d923
Ray Chen <54078384+rayzchen@users.noreply.github.com>
2022-05-28 20:58:36 +0000

    Add depth to gui

--- pyunity/shaders/gui/vertex.glsl
+++ pyunity/shaders/gui/vertex.glsl
@@ -3,6 +3,7 @@ layout (location = 0) in vec2 aPos;
 
 uniform mat4 projection;
 uniform mat4 model;
+uniform float depth;
 uniform int flipX;
 uniform int flipY;
 
@@ -10,7 +11,7 @@ out vec2 TexCoord;
 out vec3 FragPos;
 
 void main() {
-    gl_Position = projection * model * vec4(aPos, 0.0, 0.5);
+    gl_Position = projection * model * vec4(aPos, 0.0, depth);
     TexCoord = aPos;
     TexCoord *= 1 - vec2(flipX, flipY) * 2;
 }
