fefb992fddee1fe035c22df31e4b4d70e9865690
Ray Chen <tankimarshal2@gmail.com>
2023-05-31 20:09:05 +0100

    Remove usage of `egg_info` and sync module stub docstring

--- .github/workflows/build_wheel.sh
+++ .github/workflows/build_wheel.sh
@@ -2,7 +2,7 @@
 NAME=cp$(echo $1 | cut --complement -b 2)
 PYTHON=$(ls /opt/python/$NAME-$NAME*/bin/python)
 $PYTHON -m pip install -r .github/build_requirements.txt
-$PYTHON setup.py egg_info
-test -f pyunity.egg-info/version.json || exit 1
+# $PYTHON setup.py egg_info
+# test -f pyunity.egg-info/version.json || exit 1
 $PYTHON -m build -w -o tmp
 auditwheel repair -w dist tmp/*
