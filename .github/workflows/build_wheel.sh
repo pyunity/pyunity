23c4e1824636dbf21fbb40625c21af9421b1364e
Ray Chen <tankimarshal2@gmail.com>
2023-05-31 11:05:08 +0100

    Fix parallel compilation syntax

--- .github/workflows/build_wheel.sh
+++ .github/workflows/build_wheel.sh
@@ -4,5 +4,5 @@ PYTHON=$(ls /opt/python/$NAME-$NAME*/bin/python)
 $PYTHON -m pip install -r .github/build_requirements.txt
 $PYTHON setup.py egg_info
 test -f pyunity.egg-info/version.json || exit 1
-$PYTHON setup.py bdist_wheel -j 4 -d tmp
+$PYTHON setup.py build_ext -j 4 bdist_wheel -d tmp
 auditwheel repair -w dist tmp/*
