69df2ccf9048d3d740765de735aa77327dc7ab18
Ray Chen <tankimarshal2@gmail.com>
2023-05-31 14:07:07 +0100

    Use correct Python installation in docker image [ci skip]

--- .github/workflows/build_wheel.sh
+++ .github/workflows/build_wheel.sh
@@ -4,5 +4,5 @@ PYTHON=$(ls /opt/python/$NAME-$NAME*/bin/python)
 $PYTHON -m pip install -r .github/build_requirements.txt
 $PYTHON setup.py egg_info
 test -f pyunity.egg-info/version.json || exit 1
-python -m build -w -o tmp
+$PYTHON -m build -w -o tmp
 auditwheel repair -w dist tmp/*
