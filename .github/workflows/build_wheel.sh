6d41f822c3dec6d972d0d4d3dc907e0ab207ba8d
Ray Chen <tankimarshal2@gmail.com>
2023-05-31 10:57:06 +0100

    Install correct dependencies in actions

--- .github/workflows/build_wheel.sh
+++ .github/workflows/build_wheel.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 NAME=cp$(echo $1 | cut --complement -b 2)
 PYTHON=$(ls /opt/python/$NAME-$NAME*/bin/python)
-$PYTHON -m pip install "cython>=3.0.0a8" wheel
+$PYTHON -m pip install -r .github/build_requirements.txt
 $PYTHON setup.py egg_info
 test -f pyunity.egg-info/version.json || exit 1
 $PYTHON setup.py bdist_wheel -d tmp
